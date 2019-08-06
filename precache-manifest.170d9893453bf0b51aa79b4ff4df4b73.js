5a25888b8e8327009c768a53cfc0bd72daae8968
Anuar Harb <anuarharb@hotmail.com>
2019-08-05 21:43:32 -0500

    version 1!

---
+++ precache-manifest.170d9893453bf0b51aa79b4ff4df4b73.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bc1127757de00d89749abac01a802b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9985ed8f62c313bb6f6f",
+    "url": "/static/css/main.3c63bbc6.chunk.css"
+  },
+  {
+    "revision": "d2c18d1e8ac346de8e96",
+    "url": "/static/js/2.2751d29f.chunk.js"
+  },
+  {
+    "revision": "9985ed8f62c313bb6f6f",
+    "url": "/static/js/main.ba8ebbd0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1233fdf19c04333c7f58af4eb8698452",
+    "url": "/static/media/Lato-Black.1233fdf1.ttf"
+  },
+  {
+    "revision": "a2e8f8eef2ec2047a32e2d6a152a0311",
+    "url": "/static/media/Lato-Hairline.a2e8f8ee.ttf"
+  },
+  {
+    "revision": "863b7dcd5ec2c3923122af25ce0f7e4c",
+    "url": "/static/media/Lato-Medium.863b7dcd.ttf"
+  },
+  {
+    "revision": "6a61bd77d0c50110bc43611f7713f907",
+    "url": "/static/media/amigo.6a61bd77.jpg"
+  },
+  {
+    "revision": "3db7b7d3765bd422cf529684b5a69991",
+    "url": "/static/media/banda.3db7b7d3.jpg"
+  },
+  {
+    "revision": "180b48e1e2d9180870154508a60769ee",
+    "url": "/static/media/flamingo.180b48e1.png"
+  },
+  {
+    "revision": "735ab5cfb8095ff7eb53825f21e17071",
+    "url": "/static/media/libreta.735ab5cf.jpg"
+  },
+  {
+    "revision": "ca266a50785ae95a3a0f4d2cf9e4a419",
+    "url": "/static/media/libro.ca266a50.jpg"
+  },
+  {
+    "revision": "d40943b24ac6bf0b6ae984c4e8fe2021",
+    "url": "/static/media/liga.d40943b2.jpg"
+  },
+  {
+    "revision": "4ef1da67ee68fa638d5454f930368b25",
+    "url": "/static/media/snickers.4ef1da67.jpg"
+  }
+]);
